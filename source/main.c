789eea5677f3950d95d4739ac083d27604e7d353
giuseppi <giuseppipelayo@gmail.com>
2020-01-21 17:41:19 -0800

    finished lab4pt2

--- source/main.c
+++ source/main.c
@@ -3,93 +3,65 @@
 #include "simAVRHeader.h"
 #endif
 
-enum part2_states{start, init, plus, minus, reset} state;
+	enum STATES { START, INIT, INCREMENT, RESET, DECREMENT } state;
+	unsigned char holder;
 
-void part2_SM() {
-	unsigned static char val;
-	unsigned char A0 = PINA & 0x01;
-	unsigned char A1 = PINA;
-	switch(state) { // Transitions
-		case start:
-			state = init;
-			val = 0x07;
+void tick() {
+	switch(state) {
+		case START:
+			state = INIT;
+			holder = 7;
 			break;
-		case init:
-			if (A0 && A1) {
-				state = reset;
-			}
-			else if ((val >= 0) && (A1 && !A0)) {
-				state = minus;
-			}
-			else if ((val < 9) && (A0 && !A1)) {
-				state = plus;
-			}
-			else {
-				state = init;
+		case INIT:
+			if (PINA == 3) {
+				state = RESET;
+			} else if (PINA == 1) {
+				state = INCREMENT;
+			} else if (PINA == 2) {
+				state = DECREMENT;
+			} else {
+				state = INIT;
 			}
 			break;
-		case minus:
-			if (!A0 && !A1) {
-				state = init;
-			}
-			else if (A0 && A1) {
-				state = reset;
-			}
-			else {
-				state = minus;
-			}
+		case INCREMENT:
+			state = INIT;
 			break;
-		case plus:
-			if (!A0 && !A1) {
-				state = init;
-			}
-			else if (A0 && A1) {
-				state = reset;
-			}
-			else {
-				state = plus;
-			}
+		case RESET:
+			state = INIT;
 			break;
-		case reset:
-			if (A0 && A1) {
-				state = reset;
-			}
-			else {
-				state = init;
-			}
+		case DECREMENT:
+			state = INIT;
 			break;
-		default:
-			break;
-		}
-	
-	switch (state) { // State Actions			
-		case init:
-			break;
-		case minus:
-			val -= 0x01;
-			break;
-		case plus:
-			val += 0x01;
-			break;
-		case reset:
-			val = 0x00;
-			break;
-		default:
-			break;
-		
 	}
-	PORTC = val;
+
+	switch(state) {
+		case START:
+			break;
+		case INIT:
+			break;
+		case INCREMENT:
+			if (holder < 9) {
+			holder++;
+			}
+			break;
+		case RESET:
+			holder = 0;
+			break;
+		case DECREMENT:
+			if (holder > 0) {
+			holder--;
+			}
+			break;
+	}
 }
 
 int main(void) {
-    	DDRA = 0x00;
-	PORTA = 0xFF;
-	DDRC = 0xFF;
-	PORTC = 0x00;
-	state = start;
-
-    	while(1) {
-		part2_SM();
-	}
-	return 0;
+DDRA = 0x00; DDRC = 0xFF; PORTA = 0xFF; PORTC = 0x00;
+state = START;
+holder = 7;
+    while (1) {
+	tick();
+	PORTC = holder;
+    }
+    return 1;
 }
