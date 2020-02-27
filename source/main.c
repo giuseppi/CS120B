767ec32fe70051b612b3bad9d58411f4f2381289
Giuseppi Pelayo <gpela001@wch136-18.cs.ucr.edu>
2020-02-27 11:06:35 -0800

    project checkpoint1

--- source/main.c
+++ source/main.c
@@ -1,208 +1,91 @@
+#define F_CPU 8000000UL
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include "timer.h"
-#include "keypad.h"
+#include <util/delay.h>
 #include <stdio.h>
+#include "io.c"
+#include "dht.h"
 
-/*
-unsigned char GetKeypadKey() {
-	/* // Returns '\0' if no key pressed, else returns char '1', '2', ... '9', 'A', ...
-	// If multiple keys pressed, returns leftmost-topmost one
-	// Keypad must be connected to port C
-	  Keypad arrangement
-			PC4 PC5 PC6 PC7
-	   col  1   2   3   4
-	row
-	PC0 1   1 | 2 | 3 | A
-	PC1 2   4 | 5 | 6 | B
-	PC2 3   7 | 8 | 9 | C
-	PC3 4   * | 0 | # | D
-	
-	
-	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	
-	if (GetBit(PINC,0)==0) { return('1'); }
-	if (GetBit(PINC,1)==0) { return('4'); }
-	if (GetBit(PINC,2)==0) { return('7'); }
-	if (GetBit(PINC,3)==0) { return('*'); }
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
 
-	// Check keys in col 2
-	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('2'); }
-	if (GetBit(PINC,1)==0) { return('5'); }
-	if (GetBit(PINC,2)==0) { return('8'); }
-	if (GetBit(PINC,3)==0) { return('0'); }
-
-	// Check keys in col 3
-	PORTC = 0xBF; // Enable col 6 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('3'); }
-	if (GetBit(PINC,1)==0) { return('6'); }
-	if (GetBit(PINC,2)==0) { return('9'); }
-	if (GetBit(PINC,3)==0) { return('#'); }
-
-	// Check keys in col 4	
-	PORTC = 0x7F; // Enable col 7 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('A'); }
-	if (GetBit(PINC,1)==0) { return('B'); }
-	if (GetBit(PINC,2)==0) { return('C'); }
-	if (GetBit(PINC,3)==0) { return('D'); }
-
-	return('\0'); // default value
-	
-
+void TimerOn() {
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;
 }
-*/
-unsigned long int findGCD(unsigned long int a, unsigned long int b) { 
-	//--------Find GCD function --------------------------------------------------
-	unsigned long int c;
-	while(1){
-		c = a%b;
-		if(c==0){return b;}
-		a = b;
-		b = c;
-	}
-	return 0;
-	//--------End find GCD function ----------------------------------------------
-	
+void TimerOff() {
+    TCCR1B = 0x00;
+}
+void TimerISR() {
+    TimerFlag = 1;
+}
+ISR(TIMER1_COMPA_vect) {
+    _avr_timer_cntcurr--;
+    if (_avr_timer_cntcurr == 0) {
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
 }
 
-typedef struct _task {
-	//--------Task scheduler data structure---------------------------------------
-	// Struct for Tasks represent a running process in our simple real-time operating system.
-	/*Tasks should have members that include: state, period,
-		a measurement of elapsed time, and a function pointer.*/
-	signed char state; //Task's current state
-	unsigned long int period; //Task period
-	unsigned long int elapsedTime; //Time elapsed since last task tick
-	int (*TickFct)(int); //Task tick function
-	//--------End Task scheduler data structure-----------------------------------
-} task;
-
-unsigned char SM1_out;
-unsigned short numGet;
-
-//Enumeration of states.
-enum SM1_States { SM1_Start, SM1_get};
-enum SM2_States { SM2_Start, SM2_Display };
-
-
-int SMTick1(int state) {
-	switch (state) {
-		case SM1_Start: state = SM1_get; break;
-		case SM1_get: break;
-		default: state = SM1_Start; break;
-	}
-	switch (state) {
-		case SM1_Start: break;
-		case SM1_get:
-			numGet = GetKeypadKey();
-			switch (numGet) {
-				case '\0': SM1_out = 0x1F; break; // All 5 LEDs on
-				case '1': SM1_out = 0x01; break; // hex equivalent
-				case '2': SM1_out = 0x02; break;
-				case '3': SM1_out = 0x03; break;
-				case '4': SM1_out = 0x04; break;
-				case '5': SM1_out = 0x05; break;
-				case '6': SM1_out = 0x06; break;
-				case '7': SM1_out = 0x07; break;
-				case '8': SM1_out = 0x08; break;
-				case '9': SM1_out = 0x09; break;
-				case 'A': SM1_out = 0x0A; break; //ASCII 65
-				case 'B': SM1_out = 0x0B; break; //ASCII 66
-				case 'C': SM1_out = 0x0C; break; //ASCII 67
-				case 'D': SM1_out = 0x0D; break; //ASCII 68
-				case '*': SM1_out = 0x0E; break; //ASCII 42
-				case '0': SM1_out = 0x00; break;
-				case '#': SM1_out = 0x0F; break; //ASCII 35
-				default: SM1_out = 0x1B; break; // Should never occur. Middle LED off.
-			}
-			break;
-	}
-	return state;
-}
-int SMTick2(int state) {
-	switch(state) {
-		case SM2_Start:
-			state = SM2_Display;
-			break;
-		case SM2_Display: break;
-		default: state = SM2_Start; break;
-	}
-	switch (state) {
-		case SM2_Start: break;
-		case SM2_Display:
-			PORTB = SM1_out;
-			break;
-		default: break;
-	}
-	return state;
+void ADC_init() {
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
 }
 
-int main() {
+int ADC_Read(char channel)
+{
+    int ADC_value;
+    
+    ADMUX = (0x40) | (channel & 0x07);/* set input channel to read */
+    ADCSRA |= (1<<ADSC);    /* start conversion */
+    while((ADCSRA &(1<<ADIF))== 0);    /* monitor end of conversion interrupt flag */
+    
+    ADCSRA |= (1<<ADIF);    /* clear interrupt flag */
+    ADC_value = (int)ADCL;    /* read lower byte */
+    ADC_value = ADC_value + (int)ADCH*256;/* read higher 2 bits, Multiply with weightage */
 
-	DDRB = 0xFF; PORTB = 0x00; // PORTB set to output, outputs init 0s
-	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
-	// . . . etc
+    return ADC_value;        /* return digital value */
+}
 
-	// Period for the tasks
-	unsigned long int SMTick1_calc = 50;
-	unsigned long int SMTick2_calc = 50;
-
-	//Calculating GCD
-	unsigned long int tmpGCD = 1;
-	tmpGCD = findGCD(SMTick1_calc, SMTick2_calc);
-
-	//Greatest common divisor for all tasks or smallest time unit for tasks.
-	unsigned long int GCD = tmpGCD;
-
-	//Recalculate GCD periods for scheduler
-	unsigned long int SMTick1_period = SMTick1_calc/GCD;
-	unsigned long int SMTick2_period = SMTick2_calc/GCD;
-
-
-	//Declare an array of tasks
-	static task task1, task2;
-	task *tasks[] = { &task1, &task2 };
-	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
-
-	// Task 1
-	task1.state = -1;//Task initial state.
-	task1.period = SMTick1_period;//Task Period.
-	task1.elapsedTime = SMTick1_period;//Task current elapsed time.
-	task1.TickFct = &SMTick1;//Function pointer for the tick.
-
-	// Task 2
-	task2.state = -1;//Task initial state.
-	task2.period = SMTick2_period;//Task Period.
-	task2.elapsedTime = SMTick2_period;//Task current elapsed time.
-	task2.TickFct = &SMTick2;//Function pointer for the tick
-
-	// Set the timer and turn it on
-	TimerSet(GCD);
-	TimerOn();
-	
-	unsigned short i; // Scheduler for-loop iterator
-	while(1) {
-		numGet = GetKeypadKey();
-		// Scheduler code
-		for ( i = 0; i < numTasks; i++ ) {
-			// Task is ready to tick
-			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
-				// Setting next state for task
-				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
-				// Reset the elapsed time for next tick.
-				tasks[i]->elapsedTime = 0;
-			}
-			tasks[i]->elapsedTime += 1;
-		}
-		while(!TimerFlag);
-		TimerFlag = 0;
-	}
-
-	// Error: Program should not exit!
-	return 0;
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0x04;
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+    
+    char buffer[40];
+    int ADC_Value1;
+    int ADC_Value2;
+    
+    ADC_init();        /* Initialize ADC */
+    LCD_init();        /* Initialize LCD */
+    LCD_ClearScreen();
+    TimerSet(100);
+    TimerOn();
+    
+    while(1)
+    {
+        unsigned click = ~PINA & 0x04;
+        ADC_Value1 = ADC_Read(0);/* Read the status on X-OUT pin using channel 0 */
+        ADC_Value2 = ADC_Read(1);/* Read the status on Y-OUT pin using channel 0 */
+        
+        if (click) {
+            sprintf(buffer, "X=%d   Y=%d    Button pressed ", ADC_Value1, ADC_Value2);
+        }
+        else {
+            sprintf(buffer, "X=%d   Y=%d    Button release", ADC_Value1, ADC_Value2);
+        }
+        
+        LCD_DisplayString(1, buffer);
+        LCD_Cursor(24);
+        while (!TimerFlag) {};    // Wait 300ms
+        TimerFlag = 0;
+    }
+    
 }
